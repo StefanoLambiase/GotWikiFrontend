cc731a4bcf740b6ebac526522093264185ed0cd2
SteafanoLambiase <stefanolambiase96@gmail.com>
2020-07-04 17:19:16 +0200

    Started to implement CharactersTable and CharacterCard.

--- src/app/features/characters/models/characterMainInfo/character-main-info.ts
+++ src/app/features/characters/models/characterMainInfo/character-main-info.ts
@@ -1,2 +1,10 @@
 export class CharacterMainInfo {
+  name: string;
+  house: string[];
+  royal: boolean;
+  alive: boolean;
+  nickname: string;
+  actor: string;
+  sceneCount: number;
+  killCount: number;
 }
